b9cf3dd0db7d39f6f1403f16052bc4d850fa5a3d
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-18 23:34:36 +0300

    sign up page style

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -16,14 +16,16 @@ export const SignUp = () => {
     e.preventDefault();
     
   };
-  return <div className=''>
-    <h3 className=''>Create an Account</h3>
-    <p className=''>
+  return <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+    <h3 className='text-lg font-semibold text-black'>Create an Account</h3>
+    <p className='text-xs text-slate-700 mt-[5px] mb-6'>
       Join us today by entering your details below.
     </p>
 
-    <form className=''>
-      <div className=''>
+    <form onSubmit={handleSignUp}>
+
+      <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+      <div className='grid grid-cols-1 md:grid-cols-1 gap-2'>
         <Input
         value={fullName}
         onChange={({target}) => setFullName(target.value)}
@@ -50,15 +52,15 @@ export const SignUp = () => {
 
       {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p> }
 
-      <button type='submit' className=''>
+      <button type='submit' className='btn-primary'>
         SIGN UP
       </button>
 
-      <p className=''>
+      <p className='text-[13px] text-slate-800 mt-3'>
         Already an account?{" "}
 
         <button
-        className=''
+        className='font-medium text-primary underline cursor-pointer'
         onClick={() => {
           setCurrentPage("login");
         }}> 

9007aadef8de741191ef78fb690d8bb0e6aae8be
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-31 21:01:58 +0300

    CARD_BG

--- frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
+++ frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { LuPlus } from 'react-icons/lu' 
+import {CARD_BG} from '../../utils/data'
+import toast from "react-hot-toast"
+import DashoboardLayout from '../../components/Layouts/DashboardLayout'
+
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <DashoboardLayout>Dashboard</DashoboardLayout>
   )
 }
+
+export default Dashboard;
